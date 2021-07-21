eccb7d3603303c4a278b1ab285894140fd91330d
Masum Billah <49682505+drbillah@users.noreply.github.com>
2021-07-21 20:44:38 +0800

    Update and rename cmd.txt to cmd.sh

--- contiguate/cmd.sh
+++ contiguate/cmd.sh
@@ -1,3 +1,1 @@
 perl /data/Softwares/abacas.1.3.1.pl -r /data/chrom/Chr1.fasta -q /data/BBG/contig/final_contig_200.fa -p nucmer -d -o black_bengal_1
-
-Ref: https://academic.oup.com/bioinformatics/article/25/15/1968/213094
